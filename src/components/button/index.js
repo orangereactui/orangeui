8dc6c61f95337d60ad84a9a25c1ab72ba962de83
tl.zou <zoutongliang@gmail.com>
2019-02-21 13:32:15 +0800

    init project, add button component

---
+++ src/components/button/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './index.css';
+
+
+const Button = ({text}) => <button className='btn'>{text}</button>
+
+Button.propTypes = {
+  text: PropTypes.any,
+}
+
+export default Button
